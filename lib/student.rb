2e3e245dbfce340b31c270928d01fd85ae24060a
Sachin Mahajan <sachinm78@gmail.com>
2019-04-11 16:51:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,11 +29,6 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def update
-      sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-      DB[:conn].execute(sql, self.name, self.grade, self.id)
-    end
-  
   def save
     if self.id
       self.update
@@ -53,10 +48,21 @@ class Student
       student
   end
   
+  def self.new_from_db(array)
+      sql = "SELECT * FROM students WHERE name = ?, grade = ?, id = ?"
+      result = DB[:conn].execute(sql, name)[0]
+      Student.new(result[0], result[1], result[2])
+  end
+  
   def self.find_by_name(name)
       sql = "SELECT * FROM students WHERE name = ?"
       result = DB[:conn].execute(sql, name)[0]
       Student.new(result[0], result[1], result[2])
-    end
+  end
+  
+  def update
+      sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
